c78bc3450c1854d87f73b60acc9cc14bc615a412
vishaka <74041479+vishaka31@users.noreply.github.com>
2024-08-20 17:09:01 +0530

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-    "react-native-animatable": "^1.4.0",
-    "react-native-fontawesome": "^7.0.0",
-    "react-native-gesture-handler": "^2.18.1",
-    "react-native-otp-textinput": "^1.1.6",
-    "react-native-reanimated": "^3.15.0",
-    "react-native-splash-screen": "^3.3.0",
-    "react-native-toast-message": "^2.2.0",
-    
