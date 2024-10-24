44caa143036354a61dfe0aa69e8ba74f09793072
Prathisha <mprathisha2@gmail.com>
2024-10-24 17:33:07 +0530

    Prathisha-changes

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -2,16 +2,9 @@
 
 // Local
 export const API_BASE_URL = 'http://192.168.29.10:3000'; // nodejs API 
-<<<<<<< Updated upstream
-
 
 //***** Live *********
 //  export const API_BASE_URL = 'http://103.191.208.15:3000';
-=======
-// export const API_BASE_URL = 'https://API.BMWBUSINESSCLUB.COM';
-
-
->>>>>>> Stashed changes
 
 
 //  export const API_BASE_URL = 'https://API.BMWBUSINESSCLUB.COM';
