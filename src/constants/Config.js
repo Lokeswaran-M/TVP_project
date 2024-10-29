e7cfd2daa2cf6c070bbebd0c4ecaa1e84d1d18cc
Lokeswaran.M <113868084+lokeswaran007@users.noreply.github.com>
2024-10-29 14:46:38 +0530

    lokeshonemin

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -11,7 +11,6 @@ export const API_BASE_URL = 'http://192.168.29.10:3000'; // nodejs API
 // export const API_BASE_URL = 'https://API.BMWBUSINESSCLUB.COM';
 
 
->>>>>>> Stashed changes
 
 
 //  export const API_BASE_URL = 'https://API.BMWBUSINESSCLUB.COM';
