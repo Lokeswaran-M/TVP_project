6752213bf64f2113a9f1b89708ecf5ec4753a9f9
Prathisha <mprathisha2@gmail.com>
2024-11-06 15:16:35 +0530

    Prathi

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,13 +1,12 @@
 
-
 // Local
-// export const API_BASE_URL = 'http://192.168.29.10:3000'; // nodejs API 
+export const API_BASE_URL = 'http://192.168.29.10:3000'; // nodejs API 
 
 // ***** Live *********
 //  export const API_BASE_URL = 'http://103.191.208.15:3000';
 
 //***** Live *********
-export const API_BASE_URL = 'https://api.bmwbusinessclub.com';
+// export const API_BASE_URL = 'https://api.bmwbusinessclub.com';
 
 //  export const API_ENDPOINTNODE = 'http://103.191.208.15:3000/';
 
