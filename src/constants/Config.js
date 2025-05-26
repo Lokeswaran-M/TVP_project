8f4d8ff6e9222763e175fdd7a94f81bc95b383c4
Lokeswaran.M <113868084+Lokeswaran-M@users.noreply.github.com>
2025-05-26 09:35:49 +0530

    commit

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -3,8 +3,10 @@
 // Local
 // export const API_BASE_URL = 'http://192.168.29.10:3000';
 
+
 // ***** Live *********
 //  export const API_BASE_URL = 'http://103.191.208.15:3000';
+
 
 //***** Live *********
 export const API_BASE_URL = 'https://tpv.smartzensol.com';
